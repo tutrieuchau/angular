f3f0dec7ddbcb8b0f8165d6463b5beda6ab8ca53
tutrieuchau <tutrieuchau@gmail.com>
2018-01-14 19:52:38 +0700

    angular js

--- angular/affiliate/src/app/app.component.ts
+++ angular/affiliate/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Application For Afilicate';
+  title = 'Tour of Heroes';
 }
